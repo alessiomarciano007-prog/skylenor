bf881cd209be6962df8f6fd2c815c14487ac75a4
alessiomarciano007-prog <alessiomarciano007@gmail.com>
2025-10-10 19:51:09 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
 // middleware.ts
-//import { NextResponse } from 'next/server';
-//import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 // Applica a tutte le route
-//export const config = { matcher: '/:path*' };
+export const config = { matcher: '/:path*' };
 
-//export default function middleware(_req: NextRequest) {
-  // Risponde 404 a qualunque richiesta
-//  return new NextResponse('Not Found', { status: 404 });
-//}
+export default function middleware(_req: NextRequest) {
+   Risponde 404 a qualunque richiesta
+  return new NextResponse('Not Found', { status: 404 });
+}
