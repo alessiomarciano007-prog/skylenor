b986c7923a56fab5f7c162c4916a1eaca4b65f66
alessiomarciano007-prog <alessiomarciano007@gmail.com>
2025-10-11 16:19:21 +0200

    Update Section.tsx

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -17,9 +17,9 @@ export default function Section({ children, className, fade = 180 }: Props) {
     to bottom,
     rgba(0,0,0,0) 0%,
     rgba(0,0,0,0.4) 25%,
-    #000 25%,
-    #000 35%,
-    rgba(0,0,0,0.6) 75%,
+    #000 45%,
+    #000 55%,
+    rgba(0,0,0,0.4) 75%,
     rgba(0,0,0,0) 100%
   )`;
 
