55db169589ec474b080e409f4a36e77b308a93e2
alessiomarciano007-prog <alessiomarciano007@gmail.com>
2025-10-14 23:53:08 +0200

    Update Section.tsx

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -17,16 +17,16 @@ export default function Section({
   const bottomGrad =
     "linear-gradient(to bottom," +
     "rgba(255,255,255,0) 10%," +
-    "rgba(255,255,255,0.26) 40%," +
-    "rgba(255,255,255,0.77) 70%," +
+    "rgba(255,255,255,0.46) 40%," +
+    "rgba(255,255,255,0.87) 70%," +
     "#ffffff 100%)";
 
  // gradiente morbido verso l’ALTO (bianco -> trasparente)
   const topGrad =
     "linear-gradient(to top," +
     "rgba(255,255,255,0) 10%," +
-    "rgba(255,255,255,0.26) 40%," +
-    "rgba(255,255,255,0.77) 70%," +
+    "rgba(255,255,255,0.46) 40%," +
+    "rgba(255,255,255,0.87) 70%," +
     "#ffffff 100%)";
 
   return (
